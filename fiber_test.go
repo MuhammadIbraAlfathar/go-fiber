f8a019c6f770096fb06a7bab8edb6ebf4f2dd649
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-24 21:08:15 +0700

    Create test request form

--- fiber_test.go
+++ fiber_test.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -93,4 +94,23 @@ func TestRouteParameter(t *testing.T) {
 	bytes, err := io.ReadAll(response.Body)
 	assert.Nil(t, err)
 	assert.Equal(t, "Get Order 20 From User ibra", string(bytes))
+}
+
+func TestRequestForm(t *testing.T) {
+
+	app.Post("/hello", func(ctx *fiber.Ctx) error {
+		name := ctx.FormValue("name")
+		return ctx.SendString("Hello " + name)
+	})
+
+	body := strings.NewReader("name=Ibra")
+	request := httptest.NewRequest(http.MethodPost, "/hello", body)
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Ibra", string(bytes))
 }
