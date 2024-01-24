db65c8953b50f687eb094f75e6efabdd58f3831c
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-24 17:50:28 +0700

    create test router get

---
+++ fiber_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRouterGetHelloWorld(t *testing.T) {
+	app := fiber.New()
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello World")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello World", string(bytes))
+}
