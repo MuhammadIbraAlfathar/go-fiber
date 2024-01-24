63ec84a81e7581eafcacf441017e0ef2124d99a9
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-24 17:35:40 +0700

    add fiber config time out

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"time"
+)
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		IdleTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+		ReadTimeout:  time.Second * 5,
+	})
 
 	err := app.Listen("localhost:3000")
 
