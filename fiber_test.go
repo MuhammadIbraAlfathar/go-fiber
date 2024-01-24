ad86523425c30f92e5f4dfe70e1aee1e6d9adf70
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-24 18:04:54 +0700

    Create test http request

--- fiber_test.go
+++ fiber_test.go
@@ -52,3 +52,26 @@ func TestCtx(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "Hello Guest", string(bytes))
 }
+
+func TestHttpRequest(t *testing.T) {
+
+	app.Get("/request", func(ctx *fiber.Ctx) error {
+		first := ctx.Get("firstname")
+		last := ctx.Cookies("lastname")
+		return ctx.SendString("Hello " + first + " " + last)
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "/request", nil)
+	request.Header.Set("firstname", "Ibra")
+	request.AddCookie(&http.Cookie{
+		Name:  "lastname",
+		Value: "Alfathar",
+	})
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Ibra Alfathar", string(bytes))
+}
